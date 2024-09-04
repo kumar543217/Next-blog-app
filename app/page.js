319f188a86d8527f3936237c739d9d7936a66557
kumar543217 <dee96872.com>
2024-09-04 14:32:40 +0530

    i add components with ui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
+"use client"
+
+
+import BlogList from "@/Components/BlogList";
+import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
+      <BlogList/>
+      <Footer/>
     </>
   );
 }
